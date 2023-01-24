324d3f59d2b0d70d46eb385d85b3a613b83a3d32
TimmyNic <tim.nicolas@outlook.com>
2023-01-24 14:35:50 +1300

    Upgrade to PySide6.

--- mapclientplugins/fieldworkpcregtibiafibula2landmarksstep/pcregviewerwidget.py
+++ mapclientplugins/fieldworkpcregtibiafibula2landmarksstep/pcregviewerwidget.py
@@ -21,10 +21,10 @@ import os
 
 os.environ['ETS_TOOLKIT'] = 'qt4'
 
-from PySide2.QtWidgets import QDialog, QAbstractItemView, QTableWidgetItem
-from PySide2.QtGui import QIntValidator
-from PySide2.QtCore import Qt
-from PySide2.QtCore import QThread, Signal
+from PySide6.QtWidgets import QDialog, QAbstractItemView, QTableWidgetItem
+from PySide6.QtGui import QIntValidator
+from PySide6.QtCore import Qt
+from PySide6.QtCore import QThread, Signal
 
 from mapclientplugins.fieldworkpcregtibiafibula2landmarksstep.ui_pcregviewerwidget import Ui_Dialog
 from traits.api import HasTraits, Instance, on_trait_change, \
--- mapclientplugins/fieldworkpcregtibiafibula2landmarksstep/resources_rc.py
+++ mapclientplugins/fieldworkpcregtibiafibula2landmarksstep/resources_rc.py
@@ -1,9 +1,9 @@
 # Resource object code (Python 3)
 # Created by: object code
-# Created by: The Resource Compiler for Qt version 5.15.2
+# Created by: The Resource Compiler for Qt version 6.4.1
 # WARNING! All changes made in this file will be lost!
 
-from PySide2 import QtCore
+from PySide6 import QtCore
 
 qt_resource_data = b"\
 \x00\x00\x07p\
@@ -156,7 +156,7 @@ qt_resource_struct = b"\
 \x00\x00\x00T\x00\x02\x00\x00\x00\x01\x00\x00\x00\x03\
 \x00\x00\x00\x00\x00\x00\x00\x00\
 \x00\x00\x00f\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
-\x00\x00\x01y\xd4\x18]J\
+\x00\x00\x01\x85\xe1X\x0b\x0e\
 "
 
 def qInitResources():
